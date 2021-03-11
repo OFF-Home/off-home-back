e97db1406e14f957468e2bc2ae8db21e66c9692e
arnauboy <arnauboy@gmail.com>
2021-03-11 20:38:51 +0100

    Obtenir Categories - Utiliztació tres capes, falta accedir a base de dades

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+
+exports.get_categories = function(){
+    return ["Caminar", "Correr", "Senderisme", "Bicileta"];
+}
---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+
+let db = require('../models/index.js')
+
+exports.get_categories = function(req, res, next) {
+    var list_categories  = db.get_categories();
+    res.send(list_categories)};
