eac2f41d7d389e13812a7b971c2e55011a68e43b
arnauboy <arnauboy@gmail.com>
2021-03-20 12:53:19 +0100

    ObtenirActivitats - Categories amb una instància

--- src/api/categories.js
+++ src/api/categories.js
@@ -6,4 +6,11 @@ let cat = require('../services/categories.js')
 
 router.get('/', cat.get_categories);
 
+router.get('/:tagId/', function(req, res, next) {
+    // noinspection JSDeprecatedSymbols
+    cat.get_activitats(req,res,req.param('tagId'));
+});
+
+
+
 module.exports = router;
--- src/services/categories.js
+++ src/services/categories.js
@@ -1,6 +1,68 @@
 
 let db = require('../models/categories.js')
+const models = require('../../models');
 
 exports.get_categories = function(req,res,next) {
-    var categories_db  = db.get_categories();
-    res.send(categories_db)};
+    models.Categories.create({nom: "Correr", description: "algo"}).then(categories => res.send('OK'));
+    models.Categories.findAll().then(categories => {
+            res.json(categories)
+        }
+    );
+};
+
+
+
+exports.get_activitats = function(req,res,nom_categoria) {
+    /*
+    models.Categories.create({nom: "Correr", description: "algo"}).then(categories => res.send('OK'));
+    models.DataHoras.create({
+        data: "dimecres",
+        hora: "dimecres"
+
+    }).then(dat => res.send("okay"));
+
+    models.Usuaris.create({
+        email: "agnes@gmail.com",
+        username: "agnes",
+        password: "agnes",
+        birthDate: "20/4/2000",
+        description: "agnes",
+        followers: 3,
+        following: 3,
+        darkMode: 3,
+        notificacions: 3,
+        estrelles: 3,
+        tags: "agnes",
+        language: "agnes"
+    }).then(usu => res.send("okay"));
+    models.Activitats.create({
+            usuariCreador: "agnes",
+            carrer: "agnes",
+            numCarrer: "agnes",
+            dataIni: "dimecres",
+            horaIni: "dimecres",
+            categoria: "Correr",
+            maxParticipants: "agnes",
+            titol: "agnes",
+            descripcio: "agnes",
+            dataFi: "agnes",
+            horaFi: "agnes"
+        }
+    ).then(newAct => {
+        if (!newAct) {
+            return res.status(500).send({
+                message: "The activity has NOT been created :frowning: "
+            });
+        }
+        res.send('OK')
+    });
+    models.Activitats.findAll({
+        where: {
+            categoria: nom_categoria
+        }
+    }).then(activitats => {
+        res.json(activitats)
+        }
+    );*/
+    res.send(nom_categoria) //TODO això es un dummy
+};
