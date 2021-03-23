6bd94a322d6b18778e88458519f4cf9edbe2572c
arnauboy <arnauboy@gmail.com>
2021-03-23 10:04:52 +0100

    ObtenirActivitats - Categories amb dos instàncies i findAll d'activitat

--- src/services/categories.js
+++ src/services/categories.js
@@ -3,7 +3,7 @@ let db = require('../models/categories.js')
 const models = require('../../models');
 
 exports.get_categories = function(req,res,next) {
-    models.Categories.create({nom: "Correr", description: "algo"}).then(categories => res.send('OK'));
+    models.Categories.create({nom: "Nadar", description: "algo"}).then(categories => res.send('OK'));
     models.Categories.findAll().then(categories => {
             res.json(categories)
         }
@@ -13,15 +13,8 @@ exports.get_categories = function(req,res,next) {
 
 
 exports.get_activitats = function(req,res,nom_categoria) {
-    /*
-    models.Categories.create({nom: "Correr", description: "algo"}).then(categories => res.send('OK'));
-    models.DataHoras.create({
-        data: "dimecres",
-        hora: "dimecres"
 
-    }).then(dat => res.send("okay"));
-
-    models.Usuaris.create({
+   models.Usuaris.create({
         email: "agnes@gmail.com",
         username: "agnes",
         password: "agnes",
@@ -34,14 +27,37 @@ exports.get_activitats = function(req,res,nom_categoria) {
         estrelles: 3,
         tags: "agnes",
         language: "agnes"
-    }).then(usu => res.send("okay"));
+    }).then(usu => res.send("okayusu"))
+
+    models.DataHoras.create({
+        data: "27/12/2022",
+        hora: "14:00"
+
+    }).then(dat => res.send("okaydat"))
+
+    models.Categories.create({
+        nom: "correr",
+        description: "dimecres"
+
+    }).then(cat => res.send("okaycat"))
+
+    models.Llocs.create({
+        carrer: "correr",
+        numCarrer: 3,
+        maxAssistents: 3,
+        latitud: 9223372036854775807,
+        altitud: 9223372036854775806,
+
+    }).then(loc => res.send("okaylloc"))
+
+
     models.Activitats.create({
-            usuariCreador: "agnes",
-            carrer: "agnes",
-            numCarrer: "agnes",
-            dataIni: "dimecres",
-            horaIni: "dimecres",
-            categoria: "Correr",
+            usuariCreador: "agnes@gmail.com",
+            carrer: "correr",
+            numCarrer: 3,
+            dataIni: "27/12/2022",
+            horaIni: "14:00",
+            categoria: "correr",
             maxParticipants: "agnes",
             titol: "agnes",
             descripcio: "agnes",
@@ -51,11 +67,12 @@ exports.get_activitats = function(req,res,nom_categoria) {
     ).then(newAct => {
         if (!newAct) {
             return res.status(500).send({
-                message: "The activity has NOT been created :frowning: "
+                message: "The activity has NOT been created  "
             });
         }
-        res.send('OK')
+        res.send("okayACT")
     });
+
     models.Activitats.findAll({
         where: {
             categoria: nom_categoria
@@ -63,6 +80,6 @@ exports.get_activitats = function(req,res,nom_categoria) {
     }).then(activitats => {
         res.json(activitats)
         }
-    );*/
-    res.send(nom_categoria) //TODO això es un dummy
+        );
+    //res.send(nom_categoria) //TODO això es un dummy
 };
